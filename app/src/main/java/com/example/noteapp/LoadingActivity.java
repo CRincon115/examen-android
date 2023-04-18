8684952d78c6877f63a3b51b8adb34fd2c0ee6ea
Carlos Rincón <solracrincon115@gmail.com>
2023-04-18 04:11:34 -0700

    pantalla de carga nice

--- app/src/main/java/com/example/noteapp/LoadingActivity.java
+++ app/src/main/java/com/example/noteapp/LoadingActivity.java
@@ -5,18 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.QuickViewConstants;
 import android.os.Bundle;
+import android.os.Handler;
 import android.renderscript.ScriptGroup;
 import android.view.View;
-
 
 import com.bumptech.glide.Glide;
 import com.example.noteapp.databinding.ActivityLoadingBinding;
 
 
 public class LoadingActivity extends AppCompatActivity {
-
+    private static final int SPLASH_DELAY = 3000; // Tiempo en milisegundos para mostrar la pantalla de presentación
+    private Handler mHandler; // Manejador para postergar la tarea
     ActivityLoadingBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,13 +25,24 @@ public class LoadingActivity extends AppCompatActivity {
 
         Glide.with(this).load(R.drawable.notes).into(binding.NotesImage);
 
-        binding.btnComenzar.setOnClickListener(new View.OnClickListener() {
+        mHandler = new Handler(); // Crear un nuevo manejador
+
+        // Postergar la tarea de transición a la actividad principal después de un tiempo determinado
+        mHandler.postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
+            public void run() {
+                // Iniciar la actividad principal y cerrar la actividad de presentación
                 Intent intent = new Intent(LoadingActivity.this, MainActivity.class);
                 startActivity(intent);
+                finish();
             }
-        });
+        }, SPLASH_DELAY);
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Limpiar el manejador para evitar fugas de memoria
+        mHandler.removeCallbacksAndMessages(null);
     }
 }
